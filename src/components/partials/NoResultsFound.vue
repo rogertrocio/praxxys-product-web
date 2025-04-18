e838da9157eea099419f5db44a17dbb43dd016e1
Roger Trocio <rogertrocio29@gmail.com>
2025-04-18 15:05:15 +0800

    Added NoResultsFound component for products list

---
+++ src/components/partials/NoResultsFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center justify-center py-20">
+    <img src="@/assets/no-results-found.png" alt="" class="h-60">
+    <p class="text-2xl text-gray-700 font-medium mb-2">Oop!... no results found</p>
+    <p class="text-gray-700">Please try another search</p>
+  </div>
+</template>
+<script setup>
+</script>
--- src/views/product/ProductPage.vue
+++ src/views/product/ProductPage.vue
@@ -29,42 +29,46 @@
       <BaseButton type="button" label="Create" class="w-40 text-white bg-teal-600 hover:bg-teal-700" />
     </div>
 
-    <table class="w-full text-sm text-left text-gray-500 mb-6">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-        <tr>
-          <th scope="col" class="px-6 py-3 w-30"></th>
-          <th scope="col" class="px-6 py-3">Name</th>
-          <th scope="col" class="px-6 py-3">Category</th>
-          <th scope="col" class="px-6 py-3">Date and Time</th>
-          <th scope="col" class="px-6 py-3">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="product in storeProduct.products"
-          :key="product.id"
-          class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b border-gray-200">
-          <td class="px-6 py-4">
-            <div
-              class="w-14 h-10 bg-cover bg-center bg-no-repeat bg-gray-500 rounded"
-              :style="{ backgroundImage:  product.images.length > 0 ? `url(${product.images[0].url})` : `url(${ImagePlaceholder})` }">
-            </div>
-          </td>
-          <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ product.name }}</td>
-          <td class="px-6 py-4">{{ product.category.name }}</td>
-          <td class="px-6 py-4">{{ product.date_time }}</td>
-          <td class="px-6 py-4 flex items-center gap-2">
-            <BaseButton type="button" label="Edit" class="text-white bg-gray-600 hover:bg-gray-700" />
-            <BaseButton type="button" label="Delete" class="text-white bg-red-600 hover:bg-red-700" @click="showProductDeleteConfirmDialog = true" />
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <template v-if="storeProduct.products.length > 0">
+      <table class="w-full text-sm text-left text-gray-500 mb-6">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th scope="col" class="px-6 py-3 w-30"></th>
+            <th scope="col" class="px-6 py-3">Name</th>
+            <th scope="col" class="px-6 py-3">Category</th>
+            <th scope="col" class="px-6 py-3">Date and Time</th>
+            <th scope="col" class="px-6 py-3">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="product in storeProduct.products"
+            :key="product.id"
+            class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b border-gray-200">
+            <td class="px-6 py-4">
+              <div
+                class="w-14 h-10 bg-cover bg-center bg-no-repeat bg-gray-500 rounded"
+                :style="{ backgroundImage:  product.images.length > 0 ? `url(${product.images[0].url})` : `url(${ImagePlaceholder})` }">
+              </div>
+            </td>
+            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ product.name }}</td>
+            <td class="px-6 py-4">{{ product.category.name }}</td>
+            <td class="px-6 py-4">{{ product.date_time }}</td>
+            <td class="px-6 py-4 flex items-center gap-2">
+              <BaseButton type="button" label="Edit" class="text-white bg-gray-600 hover:bg-gray-700" />
+              <BaseButton type="button" label="Delete" class="text-white bg-red-600 hover:bg-red-700" @click="showProductDeleteConfirmDialog = true" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-    <ProductPagination
-      :pagination="storeProduct.pagination"
-      :current-page="currentPage"
-      @update-page="onPageChange" />
+      <ProductPagination
+        :pagination="storeProduct.pagination"
+        :current-page="currentPage"
+        @update-page="onPageChange" />
+    </template>
+
+    <NoResultsFound v-else />
 
     <ConfirmDialog
       :visible="showProductDeleteConfirmDialog"
@@ -83,6 +87,7 @@ import BaseButton from '@/components/base/BaseButton.vue'
 import BaseSelect from '@/components/base/BaseSelect.vue'
 import ImagePlaceholder from '@/assets/image-placeholder.png'
 import ProductPagination from '@/components/product/ProductPagination.vue'
+import NoResultsFound from '@/components/partials/NoResultsFound.vue'
 import ConfirmDialog from '@/components/dialogs/ConfirmDialog.vue'
 import { useProductStore } from '@/stores/product'
 import { useCommonStore } from '@/stores/common'
